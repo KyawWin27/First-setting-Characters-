f7b7fa9b879a64ff70b91c448f10a4f36dc10513
KyawWin27 <102552104+KyawWin27@users.noreply.github.com>
2023-12-16 00:01:36 -0500

    Update Character.cpp

--- Character.cpp
+++ Character.cpp
@@ -1,434 +1,326 @@
-/*
-CSCI235 Fall 2023
-Project 4 - Stew of the Day
-Georgina Woo
-June 29 2023
-Character.cpp defines the constructors and private and public function implementation of the Character class
+/*Kyaw Win
+Project Characters
+Character implementation for term Project 1
+Sep 08, 2023
 */
 
+#include <iostream>
+#include <string>
 #include "Character.hpp"
-
+#include <cctype>
 
 /**
-    Default constructor.
-    Default-initializes all private members. 
-    Default character name: "NAMELESS". 
-    Booleans are default-initialized to False. 
-    Default enum value: NONE. 
-    Default Vitality, Max Armor, and Level: 0.
-*/
-Character::Character():name_{"NAMELESS"},race_{NONE},vitality_{0},armor_{0},level_{0},enemy_{false}
-{
+      Default constructor.
+      Default-initializes all private members. 
+      Default character name: "NAMELESS". 
+      Booleans are default-initialized to False. 
+      Default enum value: NONE. 
+      Default Vitality, Max Armor, and Level: 0.
+   */
+
+Character::Character(){
+    name_ = "NAMELESS";   
+    race_ = NONE;
+    vitality_ = 0;
+    armor_ = 0;
+    level_ = 0;
+    enemy_ = false;
+
 }
 
 /**
-Parameterized constructor.
-    @param      : The name of the character (a string in UPPERCASE)
-    @param      : The race of the character (a string)
-    @param      : The character's vitality (a non-negative integer) , with default value 0
-    @param      : The character's max armor level (a non-negative integer), with default value 0
-    @param      : The character's level (a non-negative integer), with default value 0
-    @param      : A flag indicating whether the character is an enemy, with default value false
-    @post       : The private members are set to the values of the corresponding parameters. 
-                Hint: Notice the default arguments in the parameterized constructor.
-                If the given parameters are invalid, the default values are used instead.
-*/
-Character::Character(const std::string& name, const std::string& race, int vitality, int armor, int level, bool enemy) : Character()
-{
-    setName(name); // checks valid name
-    setRace(race); // checks valid race
-    (vitality >= 0) ? vitality_ = vitality: vitality_ = 0;
-    (armor >= 0) ? armor_ = armor: armor_ = 0;
-    (level >= 0) ? level_ = level: level_ = 0;
-    enemy_ = enemy; 
+  Parameterized constructor.
+  param      : The name of the character (a string in UPPERCASE)
+  param      : The race of the character (a string)
+  param      : The character's vitality (a non-negative integer) , with default  
+                value 0
+  param      : The character's max armor level (a non-negative integer), with 
+                default value 0
+  param      : The character's level (non-negative integer), with default value 0
+  param      : A flag indicating whether the character is an enemy, with default 
+                value false
+  post       : The private members are set to the values of the corresponding 
+                parameters.
+  */
+
+
+Character::Character(std::string Name, std::string race, int Vitality, int Armor, int Level, bool Enemy): 
+name_{Name}, vitality_{Vitality}, armor_{Armor}, level_{Level}, enemy_{Enemy} {
+
+
+    setName(Name);
+    enemy_ = Enemy;
+    setRace(race);
+   
+   if(Vitality >= 0) {    //checks if the vitality is negative
+    vitality_ = Vitality;       //if the vitality is negative, then it is set to a 0 because it cannot be negative
+   }
+
+   else{
+    vitality_ = 0;
+
+   }
+
+   if(Armor >= 0){      //checks if the Armor is negative
+    armor_ = Armor;         //if the armor is positive, then it is set equal
+
+   }
+
+   else{
+    armor_ = 0;
+
+   }
+
+   if(Level >= 0){     //checks if the level is negative
+
+    level_ = Level;       //if the level is npositive, then it is set equal
+
+
+   }
+
+    else{
+        level_ = 0;
+
+   }
+
 }
 
- /**
-    @param  : the name of the Character
-    @post   : sets the Character's title to the value of the parameter, in UPPERCASE. Only alphabetical characters are allowed. For example, attempting to create a character named "TW3EDLEDUM2" should create a character named "TWEDLEDUM".
-    : If the given parameter does not have any valid alphabetical characters, the character's name should be set to "NAMELESS".
+/**
+            param  : the name of the Character
+            post   : sets the Character's title to the value of the parameter, 
+                      in UPPERCASE. Only alphabetical characters are allowed. 
+                      For example, attempting to create a character named 
+                      "TW3EDLEDUM2" should create a character named "TWEDLEDUM".
+                    : If the given parameter does not have any valid alphabetical
+                      characters, the character's name should be set to "NAMELESS".
 */
-void Character::setName(const std::string& name)
-{
-    std::string nameUpper = "";
-    for(int i = 0; i<name.size();i++)
-    {
-        if(std::isalpha(name[i]))
-        {
-            nameUpper+= toupper(name[i]);
+
+
+void Character::setName(const std::string& Name){
+    std::string empt = ""; //creates an empty string for later
+
+    for(int i = 0; i < Name.length(); i++){  //loops through the parameters
+
+        if(isalpha(Name[i])){  //checks if the value of the parameter is an alphabetical character
+            empt += char(toupper(Name[i]));  //sets the empty string to the value of the parameter in uppercase and increment the value to the valid parameters
+
+
         }
+
     }
-    if(nameUpper != "")
-    {
-        name_ = nameUpper;
-    }
-    else
-    {
-        name_ = "NAMELESS";
-    }
-}
-
-
-/**
-    @return : the name of the Character
-*/
-std::string Character::getName() const
-{
-    return name_;
-}
-
-
-/**
-    @param  : the race of the Character (a string)
-    @post   : sets the Character's race to the value of the parameter. Valid races: [HUMAN, ELF, DWARF, LIZARD, UNDEAD]
-            : If the given race was invalid, set race_ to NONE.
-*/
-void Character::setRace(const std::string& race)
-{
     
-    if(race == "HUMAN")
-    {
-        race_ = HUMAN;
+    if(empt == ""){     //checks if the given parameter does not have any valid alphabetical characters
+        name_ = "NAMELESS"; //if the given value does not have any valid alphabetical characters, then it sets the name_ to be Nameless
+    } 
+    
+    else {
+        name_ = empt;  //otherwise it sets empt equal to name_
+
+
     }
-    else if(race == "ELF")
-    {
+
+
+}
+
+
+/**
+      @return : the name of the Character
+  */
+
+
+ 
+std::string Character::getName() const{
+    return name_;   //returns the name of a character
+
+
+}
+
+
+/**
+       param  : the race of the Character (a string)
+       post   : sets the Character's race to the value of the parameter.
+                 If the given race was invalid, set race_ to NONE.
+  */
+
+
+void Character::setRace(const std::string& race){
+    if(race == "HUMAN"){     //checks if the race is valid
+        race_ = HUMAN;      //if the race is valid, then it sets the character's race to the value of parameters 
+    } 
+    
+    else if(race == "ELF"){
         race_ = ELF;
-    }
-    else if(race == "DWARF")
-    {
+
+    } 
+    
+    else if(race == "DWARF"){
         race_ = DWARF;
+
     }
-    else if(race == "LIZARD")
-    {
+
+    else if(race == "LIZARD"){
         race_ = LIZARD;
+
     }
-    else if(race == "UNDEAD")
-    {
+
+    else if(race == "UNDEAD"){
         race_ = UNDEAD;
-    }
-    else
-    {
-        race_ = NONE;
-    }
-}
 
+    }
 
-/**
-    @return : the race of the Character (a string)
-*/
-std::string Character::getRace() const
-{
-    if (race_ == Race::HUMAN)
-    {
-        return "HUMAN";
+    else{
+        race_ = NONE;  //if the race is invalid, then the race is set to be None
     }
-    else if (race_ == Race::ELF)
-    {
-        return "ELF";
-    }
-    else if (race_ == Race::DWARF)
-    {
-        return "DWARF";
-    }
-    else if (race_ == Race::LIZARD)
-    {
-        return "LIZARD";
-    }
-    else if (race_ == Race::UNDEAD)
-    {
-        return "UNDEAD";
-    }
-    else
-    {
-        return "NONE";
-    }
+
 
 }
 
 
 /**
-    @param  : an integer vitality
-    @pre    : vitality >= 0 : Characters cannot have negative health
-    @post   : sets the vitality private member to the value of the parameter
-*/
-void Character::setVitality(const int& vitality)
-{
-    if (vitality >= 0)
-    {
-        vitality_ = vitality;
+      return : the race of the Character (a string)
+   */
+
+
+std::string Character::getRace()const {
+    if(race_ == NONE){  //checks if the race is valid or invalid
+        return "NONE";  //if the race is invalid, then it returns none
+
+    }
+    
+    else if(race_ == HUMAN){  //chekcs if the race is valid or invalid
+        return "HUMAN";       //if the race is valid, then it returns the character
+    }
+
+    else if(race_ == ELF){  //chekcs if the race is valid or invalid
+        return "ELF";      //if the race is valid, then it returns the character
+    }
+
+    else if(race_ == DWARF){     //chekcs if the race is valid or invalid
+        return "DWARF";          //if the race is valid, then it returns the character
+
+    }
+
+    else if(race_ == LIZARD){    //chekcs if the race is valid or invalid
+        return "LIZARD";        //if the race is valid, then it returns the character
+    }
+
+    else {    //checks if the race is valid or invalid
+        return "UNDEAD";        //if the race is valid, then it returns the character
     }
 }
 
-
 /**
-    @return : the value stored in vitality_
-*/
-int Character::getVitality() const
-{
-    return vitality_;
-}
+    param  : an integer vitality
+    pre    : vitality >= 0 : Characters cannot have negative health
+    post   : sets the vitality private member to the value of the parameter
+              if vitality is negative, do nothing.
+  */
 
-/**
-    @param  : an integer armor level
-    @pre    : armor >= 0 : Characters cannot have negative armor
-    @post   : sets the armor private member to the value of the parameter
-*/
-void Character::setArmor(const int& armor)
-{
-    if (armor >= 0)
-    {
-        armor_ = armor;
+
+void Character::setVitality(const int& Vitality){
+    if(Vitality >= 0){          //chekcs if the vitality has a positive value
+        vitality_ = Vitality;   //if the vitality is positive, then it sets the vitality private member to the value of the parameter
+
+
     }
+
 }
 
+/**
+      @return : the value stored in vitality_
+   */
+
+
+int Character::getVitality()const {
+    return vitality_;   //returns the value stored in vitality_
+
+
+}
 
 /**
-    @return : the value stored in armor_
-*/
-int Character::getArmor() const
-{
-    return armor_;
+    param  : an integer armor level
+    pre    : armor >= 0 : Characters cannot have negative armor
+    post   : sets the armor private member to the value of the parameter
+              if armor is negative, do nothing.
+  */
+
+
+
+void Character::setArmor(const int& Armor){
+    if(Armor >= 0){    //checks if the armor has a positive value
+        armor_ = Armor;   //sets armor private member to the value of the parameter
+
+
+    }
+
+
+}
+
+/**
+      return : the value stored in armor_
+   */
+
+
+
+int Character::getArmor()const {
+    return armor_;   //returns the value stored in armor_
+
+
 }
 
 /**
     @param  : an integer level
     @pre    : level >= 0 : Characters cannot have a negative
     @post   : sets the level private member to the value of the parameter
-*/
-void Character::setLevel(const int& level)
-{
-    if (level >= 0)
-    {
-        level_ = level;
+              if level is negative, do nothing.
+  */
+
+
+
+void Character::setLevel(const int& Level) {
+    if(Level >= 0){   //checks if the the level is a positive value
+        level_ = Level;   //sets level private memeber to the balue of the parameter
+
+
     }
+
+
 }
 
 
 /**
-    @return : the value stored in level_
-*/
-int Character::getLevel() const
-{
-    return level_;
+      return : the value stored in level_
+   */
+
+
+
+int Character::getLevel()const {
+    return level_;    //returns the value stored in level_
+
+
 }
 
 /**
-    @post   : sets the enemy flag to true
-*/
-void Character::setEnemy()
-{
-    enemy_ = true;
-}
+    return true if the character is an enemy, false otherwise
 
-/**
-    @return true if the character is an enemy, false otherwise
+    Note: this is an accessor function and must follow the same convention as all accessor  
+          functions even if it is not called getEnemy
+   */
 
-Note: this is an accessor function and must follow the same convention as all accessor functions even if it is not called getEnemy
-*/
-bool Character::isEnemy() const
-{
+bool Character::isEnemy()const {
     return enemy_;
-}
 
-
-
-/**
-        @param     	: A reference to the right hand side of the == operator.
-        @return     : Returns true if the right hand side character is "equal", false otherwise. 									
-                     Two characters are equal if they have the same name, same race, same level 									
-                     and are either both an enemy or not.
-
-        Note: By this definition, only some of the character's attributes must be equal for two characters to be deemed "equal".
-
-        Example: In order for character1 to be == to character2 we only need:
-        - The same name
-        - The same race
-         - The same level
-        - They must either be both an enemy or not
-*/
-bool Character::operator==(const Character& rhs) const
-{
-    return(name_ == rhs.getName() && getRace() == rhs.getRace() && level_ == rhs.getLevel() && enemy_ == rhs.isEnemy());
-}
-
-/**
-            @param     `: A reference to the right hand side of the != operator.
-            @return     : Returns true if the right hand side character is NOT "equal" (!=), false 				
-						 otherwise. Two characters are NOT equal if any of their name, race or level are
-						not equal, or if one is an enemy and the other is not.
-*/
-bool Character::operator!=(const Character& rhs) const
-{
-    return(!(*this == rhs));
-}
-
-/**
- @post     : displays Character data in the form:
-"[name_] is a Level [level_] [race_]. \nVitality: [vitality_] \nMax Armor: [armor_] \n[They are / They are not] an enemy.\n"     
-*/
-void Character::display() const
-{
-    std::cout<<name_<<" is a Level "<<level_<<" "<<getRace()<<
-    ". \nVitality: "<<vitality_<<
-    "\nMax Armor: "<<armor_<<" \n"<<
-    (enemy_ ? "They are an enemy.\n" : "They are not an enemy.\n");
-}
-
-/**
-    @pre: This function is called to execute the Action BUFF_Heal
-    @post: Increases the character's vitality by 2
-*/
-
-void Character::heal(){ 
-    // Post condition
-    vitality_ += 2;  //increases character's vitality by 2
-}
-
-/**
-  @pre: This function is called to execute the Action BUFF_MendMetal
-  @post: Increases the character's armor by 2
-
-*/
-
-void Character::mendMetal(){
-    // Post condition
-    armor_ += 2;  //increases character's armor by 2 
-}
-
-/** ATTACK ACTIONS */
-
-/**
-  @pre: This function is called to execute the Action ATT_Strike
-  @param: A pointer to a character target
-  @post: Deals 2 points of damage to the target character. 
-  If the target has armor, their armor absorbs the damage but is depleted by the same number of points. 
-  For example, if the target has 1 armor point, their armor becomes 0 and they lose 1 vitality point.
-*/
-
-void Character::strike(Character* target){
-    if(target != nullptr){
-        const int points= 2; 
-
-        if(target -> getArmor() > 0){
-            int damage = (target -> getArmor() < points) ? target -> getArmor() : points;
-            target -> setArmor(target -> getArmor() - damage);
-
-            int left;
-
-            left = points - damage;
-            target -> setVitality(target -> getVitality() - left);
-
-    }
-
-    else{
-        if((target -> getVitality() - points) <= 0){
-            target -> setVitality(0);
-
-        }
-
-        else{
-            target -> setVitality(target -> getVitality() - points);
-
-        }
-    }
-
-  }
 
 }
 
 
 /**
-  @pre: This function is called to  execute the Action ATT_ThrowTomato
-  @param: A pointer to a character target
-  @post: Deals 1 point of damage to the target character. 
-  If the target has armor, their armor absorbs the damage but is depleted by the same number of points. 
-  For example, if the target has 1 armor point, their armor becomes 0 and they don't lose any vitality points. 
-  Your character gains 1 vitality point (as laughter is the best medicine).
-*/
-
-void Character::throwTomato(Character* target){
-    if (target != nullptr){
-        const int pnts = 1;
-
-        if(target->getArmor() > 0){
-            int damage;
-
-            damage = (target->getArmor() < damage) ? target ->getArmor() : damage;
-            target->setArmor(target->getArmor() - damage);
-
-            setVitality(getVitality() + 1);
-
-        } 
-        
-        else {
-            setVitality(getVitality() + 1);
-
-        }
-
-    }
-
-}
-
-void Character::action(Action action){
-    action_queue_.push(action);
-}
+   post   : sets the enemy flag to true
+  */
 
 
-int Character::getAction() const{
-    return action_queue_.front();
-}
+void Character::setEnemy(){
+    enemy_ = true;
 
-
-void Character::PopAction(){
-    action_queue_.pop();
-}
-
-
-bool Character::Queue() const{
-    return action_queue_.empty(); 
-}
-
-void Character::ActionQueue(){
-    while(!action_queue_.empty()){
-        action_queue_.pop();
-    }
-}
-
-
-std::queue<int>& Character::getQueue(){
-    return action_queue_;
-}
-
-std::stack<Buff>& Character::getbuffStack(){
-    return buff_stack_;
-}
-
-void Character::Buff(){
-    while(!buff_stack_.empty()){
-        Buff& buff = buff_stack_.top();
-
-
-        if (buff.name_ == "BUFF_Heal"){
-            heal(); 
-        } 
-        
-        else if(buff.name_ == "BUFF_MendMetal") {
-            mendMetal();
-        }
-
-        buff.turns_--; 
-
-        
-        if (buff.turns_ == 0){
-            buff_stack_.pop();
-
-        }
-
-    }
-
-}
-
-void Character::AddbuffStack(const Buff& buff){
-    buff_stack_.push(buff); 
 
 }
